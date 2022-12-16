4070c49bd301811b36658a27f0789fbe3d1f4e99
Venkat P <venkatap@lambdastreams.com>
2022-12-15 22:11:30 -0500

    adding page object

--- src/test/java/Elements.java
+++
@@ -1,6 +0,0 @@
-public class  Elements {
-    String userName = "//*[(@id = \"user-name\")]";
-    String password = "//*[(@id = \"password\")]";
-    String loginButton = "//*[(@id = \"login-button\")]";
-    String errorHeader = "//h3";
-}
---
+++ src/test/java/org/miami/sift/LoginTest.java
@@ -0,0 +1,35 @@
+package org.miami.sift;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.miami.sift.constants.Constants;
+import org.miami.sift.pageobjects.LoginPage;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//class Name starts with Capital Letter
+// public class name needs to match File Name
+public class LoginTest {
+    ChromeDriver driver;
+    LoginPage page;
+    Constants constant = new Constants();
+
+    @Before
+    public void initPage() {
+        driver = new ChromeDriver();
+        page = new LoginPage(driver);
+    }
+
+    @After
+    public void closePage() {
+        driver.quit();
+    }
+
+    @Test
+    public void wrongPassword() {
+        driver.get(constant.baseUrl);
+        page.login(constant.userName, constant.wrongPassword);
+        Assert.assertEquals(constant.errorMessage, page.getCurrentErrorMessage());
+    }
+}
